b9f4c282e654e2f15fa1049c4dccce6c738f0221
Brendan Hughes <brendanhughes.dev@gmail.com>
2020-07-21 18:45:52 -0400

    added additional project cards

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const logo = document.querySelector("#logo path");
+//Fade In Effects
+const fadingElements = document.querySelectorAll(".fadeIn");
+const slidingElements = document.querySelectorAll(".slideIn");
+console.log(fadingElements);
+const appearOptions = {
+    threshold: 0.5,
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+},appearOptions);
 
-for (let i = 0; i<logo.length; i++) {
-    console.log("letter ${i} is ${logo[i].getTotalLength()}");
-}
+fadingElements.forEach(fadingElement => {
+    appearOnScroll.observe(fadingElement);
+});
+
+slidingElements.forEach(slidingElement => {
+    appearOnScroll.observe(slidingElement);
+});
